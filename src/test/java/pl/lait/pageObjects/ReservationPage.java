9e65dbefe4505a375beb7bb14812f969802b6837
AndrzejRokosz <andrzej_ro@wp.pl>
2018-12-01 12:51:52 +0100

    fixed bugs

--- src/test/java/pl/lait/pageObjects/ReservationPage.java
+++ src/test/java/pl/lait/pageObjects/ReservationPage.java
@@ -1,5 +1,6 @@
 package pl.lait.pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,104 +10,114 @@ import org.openqa.selenium.support.ui.Select;
 import pl.lait.selenium1.Init;
 
 public class ReservationPage {
-
+	
 	WebDriver driver;
 	
-	
-	@FindBy(xpath="html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/b/font/input[2]")
+	@FindBy(xpath = "/html/body/div/table/tbody"
+			+ "/tr/td[2]/table/tbody/tr[4]/td"
+			+ "/table/tbody/tr/td[2]/table/tbody"
+			+ "/tr[5]/td/form/table/tbody/tr[2]"
+			+ "/td[2]/b/font/input[2]")
 	WebElement oneWayRadio;
 	
-	@FindBy(xpath="html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/b/font/input[1]")
+	@FindBy(xpath = "/html/body/div/table/tbody"
+			+ "/tr/td[2]/table/tbody/tr[4]/td"
+			+ "/table/tbody/tr/td[2]/table/tbody"
+			+ "/tr[5]/td/form/table/tbody/tr[2]"
+			+ "/td[2]/b/font/input[1]")
 	WebElement roundTripRadio;
 	
-	@FindBy(name="passCount")
+	@FindBy(name = "passCount")
 	WebElement passCount;
 	
-	@FindBy(name="fromPort")
+	@FindBy(name = "fromPort")
 	WebElement fromPort;
-	
-	@FindBy(name="fromMonth")
+	@FindBy(name = "fromMonth")
 	WebElement fromMonth;
-	
-	@FindBy(name="fromDay")
+	@FindBy(name = "fromDay")
 	WebElement fromDay;
 	
-	@FindBy(name="toPort")
+	@FindBy(name = "toPort")
 	WebElement toPort;
-	
-	@FindBy(name="toMonth")
+	@FindBy(name = "toMonth")
 	WebElement toMonth;
-	
-	@FindBy(name="toDay")
+	@FindBy(name = "toDay")
 	WebElement toDay;
 	
-	@FindBy(xpath="/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[9]/td[2]/font/font/input[1]")
-	WebElement servClass;
+	@FindBy(xpath = "/html/body/div/table/tbody/"
+			+ "tr/td[2]/table/tbody/tr[4]/td/table"
+			+ "/tbody/tr/td[2]/table/tbody/tr[5]/td/"
+			+ "form/table/tbody/tr[9]/td[2]/font/font/input[1]")
+	WebElement businessClass;
 	
-	@FindBy(name="airline")
+	@FindBy(name = "airline")
 	WebElement airline;
 	
-	@FindBy(name="findFlights")
-	WebElement findFlights;
+	@FindBy(name = "findFlights")
+	WebElement continueBtn;
 	
 	public ReservationPage() {
-		driver =Init.getDriver();
+		driver = Init.getDriver();
 		PageFactory.initElements(driver, this);
-				
 	}
 	
 	public void oneWayRadioClick() {
 		oneWayRadio.click();
 	}
-	
 	public void roundTripRadioClick() {
 		roundTripRadio.click();
 	}
+	
+	public void fromPort(String portValue) {
+		Select p = new Select(fromPort);
+		p.selectByValue(portValue);
+	}
+	
 	public void passengersCount(String count) {
 		Select passCountSelect = new Select(passCount);
 		passCountSelect.selectByValue(count);
 	}
 	
-	public void selectFromPort(String port) {
-		Select fromPortSelect=new Select(fromPort); 
-			fromPortSelect.selectByValue(port);
+	public void on(String month, String day) {
+		Select m = new Select(fromMonth);
+		m.selectByValue(month);
+		Select d = new Select(fromDay);
+		d.selectByValue(day);
+		
 	}
 	
-	public void selectFromMonth(String month) {
-		Select fromMonthSelect=new Select(fromMonth);
-		fromMonthSelect.selectByValue(month);
+	public void to(String month, String day) {
+		Select m = new Select(toMonth);
+		m.selectByValue(month);
+		Select d = new Select(toDay);
+		d.selectByValue(day);
 	}
 	
-	public void selectFromDay(String day) {
-		Select fromDaySelect=new Select(fromDay);
-		fromDaySelect.selectByValue(day);
+	public void toPort(String portValue) {
+		Select p = new Select(toPort);
+		p.selectByValue(portValue);
 	}
 	
-	public void selectToPort(String port) {
-		Select toPortSelect=new Select(toPort); 
-			toPortSelect.selectByValue(port);
+	
+	public void businessClass() {
+		businessClass.click();
+	}
+	
+	/**
+	 * <select name="airline"><br>
+	 *No Preference<br>
+	 *Blue Skies Airlines<br>
+	 *Unified Airlines<br>
+	 *Pangea Airlines<br>
+	 * @param line
+	 */
+	public void airline(String line) {
+		Select l = new Select(airline);
+		l.selectByVisibleText(line);
+	}
+	
+	public void continueBtnClick() {
+		continueBtn.click();
 	}
 
-	public void selectToMonth(String month) {
-		Select toMonthSelect=new Select(toMonth);
-		toMonthSelect.selectByValue(month);
-	}
-	
-	public void selectToDay(String day) {
-		Select toDaySelect=new Select(toDay);
-		toDaySelect.selectByValue(day);
-	}
-	
-	public void serviceClass() {
-		servClass.click();
-	}
-	
-	public void selectAirline(String airLine) {
-		Select airlineSelect=new Select(airline);
-		airlineSelect.selectByValue(airLine);
-	}
-	
-	public void 
-	
-	
 }
